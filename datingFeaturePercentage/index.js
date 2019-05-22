a5bda068254164a627bc59b776b1f32b0f31e62c
Wouter Bijnsdorp <wouterb98@gmail.com>
2019-05-22 16:16:25 +0200

    Additions & Deletions
    Image added, useless files deleted

--- datingFeaturePercentage/index.js
+++ datingFeaturePercentage/index.js
@@ -1,8 +1,49 @@
-'use strict';
-module.exports = (input, options = {}) => {
-    if (typeof input !== 'string') {
-        throw new TypeError(`Expected a string, got ${typeof input}`);
-    }
+/* eslint-disable semi */
 
-    return input + ' & ' + (options.postfix || 'rainbows');
-};
+var express = require('express')
+
+var data = []
+
+express()
+    .get('/', test)
+    .get('/about', about)
+    .get('/login', login)
+    .use(notFound)
+    .listen(8000)
+
+function test(req, res) {
+    var doc = '<!doctype html>'
+
+    doc += '<title>My website</title>'
+    doc += '<h1>Is working!</h1>'
+
+    res.send(doc)
+
+}
+
+function about(req, res) {
+    var doc = '<!doctype html>'
+
+    doc += '<title>Over OKBier</title>'
+    doc += '<h1>OKBier</h1>'
+
+    res.send(doc)
+}
+
+function login(req, res) {
+    var doc = '<!doctype html>'
+
+    doc += '<title>Login</title>'
+    doc += '<h1>wip</h1>'
+
+    res.send(doc)
+}
+function notFound(req, res) {
+  var doc = '<!doctype html>'
+
+  doc += '<title>Not found</title>'
+  doc += '<h1>Not found</h1>'
+  doc += '<p>Uh oh! We couldn’t find this page!</p>'
+
+  res.status(404).send(doc)
+}
