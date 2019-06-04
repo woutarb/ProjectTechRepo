8e08473705d68a706e2958e9c7512213ad76de0c
Wouter Bijnsdorp <wouterb98@gmail.com>
2019-06-04 14:09:11 +0200

    Changed location wiki images

--- datingFeaturePercentage/server.js
+++ datingFeaturePercentage/server.js
@@ -4,47 +4,39 @@ var data = [];
 
 console.log('Server is running! port:', 8000);
 
+app.use(express.static('static'));
+
 express()
     .get('/', test)
-    .get('/about', about)
-    .get('/login', login)
+    .get('/config', config)
+    .get('/home', home)
     .use(notFound)
     .listen(8000);
 
 function test(req, res) {
     var doc = '<!doctype html>';
-
     doc += '<title>My website</title>';
     doc += '<h1>Is working!</h1>';
-
-    res.send(doc);
-
-}
-
-function about(req, res) {
-    var doc = '<!doctype html>';
-
-    doc += '<title>Over OKBier</title>';
-    doc += '<h1>OKBier</h1>';
-
     res.send(doc);
 }
 
-function login(req, res) {
-    var doc = '<!doctype html>';
+function config(req, res) {
+	var page = express.static('static',[config.html]);
+	res.send(page);
+}
 
-    doc += '<title>Login</title>';
+
+function home(req, res) {
+    var doc = '<!doctype html>';
+    doc += '<title>Homepage</title>';
     doc += '<h1>wip</h1>';
-
     res.send(doc);
 }
 
 function notFound(req, res) {
     var doc = '<!doctype html>';
-
     doc += '<title>Not found</title>';
-    doc += '<h1>Not found</h1>';
+    doc += '<h1>Page was not found</h1>';
     doc += '<p>Uh oh! We couldn’t find this page!</p>';
-
     res.status(404).send(doc);
 }
