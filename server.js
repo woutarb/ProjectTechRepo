ca2a590a0653be6491d10f3688502a049962dcb7
Wouter Bijnsdorp <wouterb98@gmail.com>
2019-06-20 12:38:17 +0200

    WIP
    Merging of server index. Less ejs partials

--- server.js
+++ server.js
@@ -1,12 +1,37 @@
 // importing depencies
 var express = require('express');
 var path = require('path');
+var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var app = express();
-
+var router = express.Router();
 console.log('Server is running! port:', 8000);
+require('dotenv').config();
 
-var indexRoute = require('./routes/index');
+mongo.MongoClient.connect(url, function(err, client){
+	if(err){
+		throw err
+	}
+	db = client.db(process.env.DB_SERVER)};
+
+	
+	
+	url = ("mongodb+srv://" + process.env.DB_USERNAME + ":" + process.env.DB_PASSWORD + "@" + process.env.DB_SERVER + "/test?retryWrites=true&w=majority", { useNewUrlParser: true });
+
+var db = mongoose.connection;
+
+//Source check: https://www.youtube.com/watch?v=cVYQEvP-_PA
+
+//Check:
+db.once("open", function(){
+ console.log("Connected to MongoDb");
+});
+
+// Check connection for errors in db
+db.on("error", function(err){
+ console.log(err);
+});
+
 
 // assigning express dependencies
 app
@@ -15,18 +40,25 @@ app
 	.use(express.json())
 	.use(express.urlencoded({extended: false}));
 
-
 app
 	.use(express.static('static'))
 	.set('view engine', 'ejs')
 	.set('views', 'views')
-// assigning the routes
-	.use('/', indexRoute)
-//Error handeling
+	// assigning the routes
+	.use('/index', index)
+	.use('/', config)
+	// error handeling
     .use(notFound)
     .listen(8000);
 
 
+
+function config(req, res){
+	res.render('config');
+}
+function index(req, res){
+	res.render('index', {users: db.collection('users').find().toArray()});
+}
 
 
 //error handeling, when the page is not found
@@ -38,3 +70,4 @@ function notFound(req, res) {
     res.status(404).send(doc);
 }
 
+module.exports = router;
